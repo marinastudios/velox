b620be77666fd7cf928e30bea8cc40977ed96d8d
MaverickQuill <VScode@toxik.cf>
2022-05-15 16:41:27 -0400

    remove random crap

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,6 +1,4 @@
 <script context="module">
-    // https://kit.svelte.dev/docs#loading
-    // context=module allows you to run code that would usually be ran on a server and return svelte props
 	/** @type {import('@sveltejs/kit').ErrorLoad} */
 	export function load({ error, status }) {
 		return {
@@ -12,12 +10,10 @@
 </script>
 
 <script>
-    // normal svelte code
 	export let title;
 </script>
 
 <h1>{title}</h1>
 <svelte:head>
-    <!-- This allows you to change the header of the page https://svelte.dev/docs#template-syntax-svelte-head -->
     <title>Error: {title}</title>
 </svelte:head>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,21 +2,10 @@
 	import '../app.css';
 	import { onMount } from 'svelte'
 	import nightwind from '$lib/nwpp'
-    /*
-        Dropdown menu, show/hide based on menu state.
-
-        Entering: "transition ease-out duration-200"
-            From: "transform opacity-0 scale-95"
-            To: "transform opacity-100 scale-100"
-          Leaving: "transition ease-in duration-75"
-            From: "transform opacity-100 scale-100"
-            To: "transform opacity-0 scale-95"
-    */
+    
 	onMount(()=>{
         nightwind.mount();
     })
-
-    $: console.log(`changed .dark ${(() => {let value;nightwind.dark.subscribe(_ => value = _);return value;})()}`);
 </script>
 
 <slot />
